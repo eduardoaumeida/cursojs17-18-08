e87ae965f721bff1dc81a71e74d6545133a3e7df
Eduardo Almeida <eduardoaumeida@yahoo.com>
2019-08-17 14:53:03 -0300

    Meu Primeiro Commit do Curso JavaScript 17e18-08

---
+++ aula1.js
@@ -0,0 +1,19 @@
+console.log("Nada de Olá hj");
+console.warn("Eu sou um alerta de erro - CÚ");
+//alert("Agora eu programo!!!!!");
+console.error("Eu sou um alerta de erro - cuidado");
+
+var idade = 28; //inteiro
+var nome = "David"; //string
+var salario = 980.99; //double ou float
+var pais = ["Oscar", "Dona Cida"];
+var veiculo = {
+                tipo: "Motocicleta"}
+                ano: 2012
+
+var relatorio = nome + " tem " + idade + "  anos e recebe " + salario + salario + "de salario";
+var relatorio = `${nome} tem ${idade}anos e recebe `;
+
+console.log(idade);
+console.log(nome);
+console.log(relatorio);
